462ba392c4f7ac76a19e8635a36da7d2fe5f2557
Kevin Krato <kratok08@gmail.com>
2019-03-14 11:51:39 -0600

    Add group by property

--- answers.js
+++ answers.js
@@ -30,5 +30,41 @@ let total = c.reduce( (accum, object) => {
 
 total;
 
-// //Sum the values of an Object with unique keys
-// var d = [{x: 1}, {x: 2}, {x: 3}]
+//4 Sum the values of an Object with unique keys
+var d = [{a: 5}, {b: 8}, {c: 3}]
+
+let total = d.reduce( (total, element) => {
+	return total += parseInt(Object.values(element));
+}, 0)
+
+/* 
+Annotation: Object.values() returns an array of all values, if we then
+try to sum all the array values, we create a string. Calling parseInt
+on the Object's output array turns every element we work through into an integer before
+passing adding it to the total.
+*/
+
+//5 Count instances of values in an Object
+var names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+
+let countedNames = names.reduce( (accum, curr) => {
+	if (curr in accum) {
+		accum[curr]++
+	} else {
+		accum[curr] = 1;
+	}
+	return accum;
+}, {})
+
+countedNames;
+
+/*
+Annotation: We reduce the array and check if the current element exists on
+the accumulator, if it does: ++ the key's value. if it doesnt: create a new 
+key and assign it a value of 1;
+*/
+
+
+
+
+
